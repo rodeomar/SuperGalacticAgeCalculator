1193e18dd8aea16dfe2e25e3a6f132b31a252a3c
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-04-15 20:04:59 +0530

    Complete Red Phase

---
+++ SuperGalacticAgeCalculator.js
@@ -0,0 +1,31 @@
+class SuperGalacticAgeCalculator {
+  constructor(age) {
+    this.age = age;
+  }
+
+  calculateAgeOnMercury() {
+    // TODO: Implement this method
+  }
+
+  calculateAgeOnVenus() {
+    // TODO: Implement this method
+  }
+
+  calculateAgeOnMars() {
+    // TODO: Implement this method
+  }
+
+  calculateAgeOnJupiter() {
+    // TODO: Implement this method
+  }
+
+  calculateYearsSincePastBirthday(planet, birthday) {
+    // TODO: Implement this method
+  }
+
+  calculateYearsUntilFutureBirthday(planet, age, futureAge) {
+    // TODO: Implement this method
+  }
+}
+
+module.exports =  SuperGalacticAgeCalculator ;
---
+++ SuperGalacticAgeCalculator.test.js
@@ -0,0 +1,71 @@
+const SuperGalacticAgeCalculator = require('./SuperGalacticAgeCalculator');
+
+describe('SuperGalacticAgeCalculator', () => {
+  describe('calculateAgeOnMercury', () => {
+    it('should return the age in Mercury years', () => {
+      const age = 30;
+      const calculator = new SuperGalacticAgeCalculator(age);
+      const mercuryAge = calculator.calculateAgeOnMercury();
+      expect(mercuryAge).toEqual(125);
+    });
+  });
+
+  describe('calculateAgeOnVenus', () => {
+    it('should return the age in Venus years', () => {
+      const age = 30;
+      const calculator = new SuperGalacticAgeCalculator(age);
+      const venusAge = calculator.calculateAgeOnVenus();
+      expect(venusAge).toEqual(48.39);
+    });
+  });
+
+  describe('calculateAgeOnMars', () => {
+    it('should return the age in Mars years', () => {
+      const age = 30;
+      const calculator = new SuperGalacticAgeCalculator(age);
+      const marsAge = calculator.calculateAgeOnMars();
+      expect(marsAge).toEqual(15.96);
+    });
+  });
+
+  describe('calculateAgeOnJupiter', () => {
+    it('should return the age in Jupiter years', () => {
+      const age = 30;
+      const calculator = new SuperGalacticAgeCalculator(age);
+      const jupiterAge = calculator.calculateAgeOnJupiter();
+      expect(jupiterAge).toEqual(2.53);
+    });
+  });
+
+  describe('calculateYearsSincePastBirthday', () => {
+    it('should return the years passed on each planet since past birthday', () => {
+      const age = 56;
+      const birthday = 43;
+      const calculator = new SuperGalacticAgeCalculator(age);
+      const yearsPassed = calculator.calculateYearsSincePastBirthday(birthday);
+      expect(yearsPassed).toEqual({
+        earth: 13,
+        mercury: 54.16,
+        venus: 20.96,
+        mars: 6.91,
+        jupiter: 1.09
+      });
+    });
+  });                                           
+
+  describe('calculateYearsUntilFutureBirthday', () => {
+    it('should return the years remaining on each planet until future birthday', () => {
+      const age = 56;
+      const futureAge = 61;
+      const calculator = new SuperGalacticAgeCalculator(age);
+      const yearsRemaining = calculator.calculateYearsUntilFutureBirthday(futureAge);
+      expect(yearsRemaining).toEqual({
+        earth: 5,
+        mercury: 20.83,
+        venus: 8.06,
+        mars: 2.65,
+        jupiter: 0.42
+      });
+    });
+  });
+});
