1b2259943ae377399c73ec009c52e2faf3b7d840
Raed Alkhanbashi <raed.alkhanbashi@gmail.com>
2023-04-15 22:06:51 +0530

    Refactored code to remove unused parseFloat function calls from the calculateYearsSincePastBirthday and calculateYearsUntilFutureBirthday functions. Additionally, added a new helper function to improve code readability.

--- SuperGalacticAgeCalculator.js
+++ SuperGalacticAgeCalculator.js
@@ -11,52 +11,55 @@ class SuperGalacticAgeCalculator {
   }
 
 
+  // Helper functions 1
+  calculateAccurateAge(planet) {
+    return this.age / this.planets[planet];
+  }
 
+  // Helper Function 2
   roundDown(value) { // For rounding down the float value
     return Math.floor(value * 100) / 100;
   }
 
   calculateAgeOnMercury() {
-    return +(this.age / .24).toFixed(2);
+    return +this.calculateAccurateAge("Mercury").toFixed(2);
   }
 
   calculateAgeOnVenus() {
-    return +(this.age / .62).toFixed(2);
+    return +this.calculateAccurateAge("Venus").toFixed(2);
 
   }
 
   calculateAgeOnMars() {
-    return +(this.age / 1.88).toFixed(2);
+    return +this.calculateAccurateAge("Mars").toFixed(2);
 
   }
 
   calculateAgeOnJupiter() {
-    return +(this.age / 11.86).toFixed(2);
+    return +this.calculateAccurateAge("Jupiter").toFixed(2);
 
   }
 
   calculateYearsSincePastBirthday(birthAge) {
 
-    const earthYearsPassed = this.age - birthAge;
+    const earthYearsPassedAges = new SuperGalacticAgeCalculator(this.age - birthAge);
     const passedOnPlanets = {};
 
+
     for (const planet in this.planets) {
-      passedOnPlanets[planet] = parseFloat(
-        this.roundDown(earthYearsPassed / this.planets[planet])
-      );
+      passedOnPlanets[planet] = this.roundDown(earthYearsPassedAges.calculateAccurateAge(planet));
     }
 
     return passedOnPlanets;
   }
 
   calculateYearsUntilFutureBirthday(futureAge) {
-    const earthYearsLeft = futureAge - this.age;
+    const earthYearsLeftAges = new SuperGalacticAgeCalculator(futureAge - this.age);
     const leftOnPlanets = {};
 
     for (const planet in this.planets) {
-      leftOnPlanets[planet] = parseFloat(
-        this.roundDown(earthYearsLeft / this.planets[planet])
-      );
+      leftOnPlanets[planet] = this.roundDown(earthYearsLeftAges.calculateAccurateAge(planet));
+
     }
 
     return leftOnPlanets;
